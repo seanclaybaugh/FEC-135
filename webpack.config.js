e2f0aebf22017e4d990cd7c9b8d8c4677cc275d3
seanclaybaugh <sdclaybaugh@gmail.com>
2021-06-04 15:28:38 -0700

    Test conflict merge

--- webpack.config.js
+++ webpack.config.js
@@ -2,33 +2,33 @@ const webpack = require('webpack');
 const path = require('path');
 
 const config = {
-  entry: path.resolve(__dirname, 'client', 'src', 'index.js'),
+  entry: path.resolve(__dirname, 'client', 'src', 'index.jsx'),
   output: {
     path: path.resolve(__dirname, 'client', 'dist'),
-    filename: 'bundle.js'
+    filename: 'bundle.js',
   },
   module: {
     rules: [
       {
         test: /\.(js|jsx)$/,
         use: 'babel-loader',
-        exclude: /node_modules/
+        exclude: /node_modules/,
       },
       {
         test: /\.css$/,
         use: [
           'style-loader',
-          'css-loader'
-        ]
-      }
-    ]
+          'css-loader',
+        ],
+      },
+    ],
   },
   resolve: {
     extensions: [
       '.js',
-      '.jsx'
-    ]
-  }
+      '.jsx',
+    ],
+  },
 };
 
 module.exports = config;
