1bb9d7077c6dab24297bfc0954053875d06e60d0
Elizabeth Phung <phung.ej@gmail.com>
2021-06-03 17:04:34 -0700

    Minor refactor of path.join line on static file

--- server/index.js
+++ server/index.js
@@ -6,7 +6,7 @@ const port = 3000;
 
 app.use(express.json());
 
-app.use(express.static(path.join(__dirname + '/../client/dist'))
+app.use(express.static(path.join(__dirname, '/../client/dist'))
 
 app.listen(port, () => {
   console.log(`Server listening on port ${3000}`);
