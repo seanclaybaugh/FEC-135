2944255117ccd882e50306683367395fb9d290b2
Iris <irisxjiang@gmail.com>
2021-06-07 14:49:17 -0700

    added questionsAnswers component directory

--- client/src/components/App.jsx
+++ client/src/components/App.jsx
@@ -1,9 +1,11 @@
 import React, { useState } from 'react';
+import QuestionsAnswers from './questions-answers/QuestionsAnswers';
 
 function App() {
   return (
     <>
       <h1>What up my Quadratic Quokkas Q.Q</h1>
+      <QuestionsAnswers />
     </>
   );
 }
---
+++ client/src/components/questions-answers/QuestionsAnswers.jsx
@@ -0,0 +1,21 @@
+import React from 'react';
+
+class QuestionsAnswers extends React.Component {
+
+  constructor(props) {
+    super(props)
+  }
+
+
+  render() {
+    return (
+      <>
+      <b>hello</b>
+      </>
+    )
+  }
+
+}
+
+
+export default QuestionsAnswers;
