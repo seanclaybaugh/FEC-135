d37e42eeca6a7cc34dc623f785370b1c5f71b84f
Iris <irisxjiang@gmail.com>
2021-06-06 21:05:15 -0700

    used api as the path prefix

--- server/router.js
+++ server/router.js
@@ -5,18 +5,22 @@ const config = require('../config');
 const atelierUrl = 'https://app-hrsei-api.herokuapp.com/api/fec2/hr-sfo/';
 const authToken = config.token;
 
-// 'atelier' is a path prefix for forwarding requests to atelier
+// 'api' is a path prefix for forwarding requests to api
 // otherwise, without this path prefix, no other routes will match
 // (e.g. static files on the route '/')
 // '*' is the wildcard route that will handle all the incoming requests
-router.all('/atelier/*', async (req, res) => {
-  console.log(`${req.method}: ${req.url}`);
+router.all('/api/*', async (req, res) => {
+  // console.log(`${req.method}: ${req.url}`);
+  console.log('req.method:')
+  console.log(req.method)
+  console.log('req.url:')
+  console.log(req.url)
 
-  // this returns an array of the path components (ex: ['atelier', 'products' 'product_id'])
+  // this returns an array of the path components (ex: ['', 'api', 'products' 'product_id'])
   const pathComponents = req.url.split('/');
   console.log('pathComponents:')
   console.log(pathComponents)
-  // we want everything but the first element of the array
+  // we want everything but the first two elements of the array
   const apiPath = pathComponents.slice(2).join('/');
   console.log('apiPath:');
   console.log(apiPath)
@@ -36,9 +40,11 @@ router.all('/atelier/*', async (req, res) => {
       headers: { Authorization: authToken },
       data: req.body,
     });
+    console.log('results.data is sending back:')
+    console.log(results.data)
     res.status(results.status).json(results.data);
   } catch (err) {
-    // console.log(err);
+    console.log(err);
     res.sendStatus(500);
   }
 });
