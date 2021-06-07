47cfb3da8cd11367d1fd507e46bcfc51c23a8a87
Iris <irisxjiang@gmail.com>
2021-06-06 23:26:52 -0700

    comments and linter fixes

--- server/router.js
+++ server/router.js
@@ -5,33 +5,35 @@ const config = require('../config');
 const atelierUrl = 'https://app-hrsei-api.herokuapp.com/api/fec2/hr-sfo/';
 const authToken = config.token;
 
-// 'api' is a path prefix for forwarding requests to api
+// router.all is routing all the requests coming to '/api/, whether its get, post, put, or delete
+
+// '/api' is a path prefix for forwarding requests to api
 // otherwise, without this path prefix, no other routes will match
 // (e.g. static files on the route '/')
 // '*' is the wildcard route that will handle all the incoming requests
 router.all('/api/*', async (req, res) => {
   // console.log(`${req.method}: ${req.url}`);
-  console.log('req.method:')
-  console.log(req.method)
-  console.log('req.url:')
-  console.log(req.url)
+  console.log('req.method:');
+  console.log(req.method);
+  console.log('req.url:');
+  console.log(req.url);
 
   // this returns an array of the path components (ex: ['', 'api', 'products' 'product_id'])
   const pathComponents = req.url.split('/');
-  console.log('pathComponents:')
-  console.log(pathComponents)
+  console.log('pathComponents:');
+  console.log(pathComponents);
   // we want everything but the first two elements of the array
   const apiPath = pathComponents.slice(2).join('/');
   console.log('apiPath:');
-  console.log(apiPath)
+  console.log(apiPath);
 
   // construct the complete request url
   const requestUrl = atelierUrl + apiPath;
-  console.log('requestUrl:')
+  console.log('requestUrl:');
   console.log(requestUrl);
 
-  console.log('body')
-  console.log(req.body)
+  console.log('body');
+  console.log(req.body);
 
   try {
     const results = await axios({
@@ -40,8 +42,8 @@ router.all('/api/*', async (req, res) => {
       headers: { Authorization: authToken },
       data: req.body,
     });
-    console.log('results.data is sending back:')
-    console.log(results.data)
+    console.log('results.data is sending back:');
+    console.log(results.data);
     res.status(results.status).json(results.data);
   } catch (err) {
     console.log(err);
@@ -51,4 +53,5 @@ router.all('/api/*', async (req, res) => {
 
 module.exports = router;
 
-// client side example: axios.get('http://localhost:3000/atelier/products')
+// client side example: axios.get('/api/products')
+// axios.post('/api/reviews?product_id=${product.id}', req.body)
