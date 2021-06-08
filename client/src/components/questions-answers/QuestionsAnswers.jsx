e8037ff3ae79907226b39d927fc4ee1e1656cfa6
Iris <irisxjiang@gmail.com>
2021-06-07 22:26:20 -0700

    refactor QuestionsAnwers.jsx to use hooks, cleaned up questionsList directory

--- client/src/components/questions-answers/QuestionsAnswers.jsx
+++ client/src/components/questions-answers/QuestionsAnswers.jsx
@@ -1,56 +1,75 @@
-import React from 'react';
-import QuestionsList from './QuestionsList';
+import React, { useState, useEffect } from 'react';
+import QuestionsList from './questionsList/QuestionsList';
 import axios from 'axios';
 
+function QuestionsAnswers() {
 
-class QuestionsAnswers extends React.Component {
+  const [data, setData] = useState({questions: []});
 
-  constructor(props) {
-    super(props)
-
-    this.state = {
-      questions: [],
-      answers: [],
-      productId: ''
-    }
-
-    this.getData = this.getData.bind(this);
-  }
-
-  getData() {
+  useEffect(() => {
     axios.get('/api/qa/questions?product_id=25171')
     .then((res) => {
-      console.log(res.data)
+      console.log(res.data.results)
+      setData({questions: res.data.results});
+    });
+  }, []);
 
-      const questionsList = res.data.results;
-      const id = res.data.product_id;
-
-      this.setState({
-        questions: questionsList,
-        productId: id
-      })
-    })
-  }
-
-  componentDidMount() {
-    this.getData();
-  }
-
-  render() {
-    return (
-      <>
-      <b>hello from questions and answers</b>
-      <br/>
-      <br/>
-      <br/>
-      <QuestionsList
-      questions={this.state.questions}
-      />
-      </>
-    )
-  }
-
+  return (
+    <>
+    <QuestionsList
+      questions={data.questions}
+    />
+    </>
+  )
 }
+// class QuestionsAnswers extends React.Component {
+
+//   constructor(props) {
+//     super(props)
+
+//     this.state = {
+//       questions: [],
+//       answers: [],
+//       productId: ''
+//     }
+
+//     this.getData = this.getData.bind(this);
+//   }
+
+//   getData() {
+//     axios.get('/api/qa/questions?product_id=25171')
+//     .then((res) => {
+//       console.log(res.data)
+
+//       const questionsList = res.data.results;
+//       const id = res.data.product_id;
+
+//       this.setState({
+//         questions: questionsList,
+//         productId: id
+//       })
+//     })
+//   }
+
+//   componentDidMount() {
+//     this.getData();
+//   }
+
+//   render() {
+//     return (
+//       <>
+//       <b>hello from questions and answers</b>
+//       <br/>
+//       <br/>
+//       <br/>
+//       <QuestionsList
+//       questions={this.state.questions}
+//       />
+//       </>
+//     )
+//   }
+
+// }
 
 
 export default QuestionsAnswers;
--- client/src/components/questions-answers/questionsList/QuestionListItem.jsx
+++ client/src/components/questions-answers/questionsList/QuestionListItem.jsx
@@ -3,18 +3,10 @@ import AnswersPerQuestion from './AnswersPerQuestion';
 
 const QuestionListItem = props => {
 
-  // console.log('ANSWERS')
-  // // console.log(props.question.answers)
   let answers = props.question.answers;
-
-  // Object.keys(answers).map((key) => {
-  //   console.log(answers[key].body)
-  // })
-
 
   return (
     <>
-
       <div>
         <li className="QuestionItem">
           Q: {props.question.question_body}
