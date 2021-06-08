34be43a940fdbd66d1835bb22c243b5c3c9629b3
seanclaybaugh <sdclaybaugh@gmail.com>
2021-06-08 10:27:14 -0700

    first get complete

--- client/src/components/reviews/reviewsIndex.jsx
+++ client/src/components/reviews/reviewsIndex.jsx
@@ -7,9 +7,20 @@ Review List
 AddReview
 
 */
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
 
-function reviewsIndex() {
+function reviewsIndex(props) {
+  const [reviews, setReviews] = useState([]);
+
+  useEffect(() => {
+    const getReviews = async () => {
+      const result = await axios.get(`/api/reviews?product_id=${props.productId}&page=1&count=20&sort=relevant`);
+      setReviews(result.data);
+    };
+    getReviews();
+  }, []);
+
   return (
     <>
       <h3>Reviews</h3>
