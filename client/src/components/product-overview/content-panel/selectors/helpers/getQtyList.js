86a81a2551e2da43e2f4b4b2707b4edcbfba25a3
Elizabeth Phung <phung.ej@gmail.com>
2021-06-09 11:46:19 -0700

    Refactored getQtyList helper func

--- client/src/components/product-overview/content-panel/selectors/helpers/getQtyList.js
+++ client/src/components/product-overview/content-panel/selectors/helpers/getQtyList.js
@@ -1,11 +1,11 @@
 const getQtyList = (qty) => {
   let options = [];
-  let count = 1;
+  let int = 1;
   let max = qty < 15 ? qty : 15;
 
-  while (max >= count) {
-    options.push(count);
-    count += 1;
+  while (max >= int) {
+    options.push(int);
+    int += 1;
   }
 
   return options;
