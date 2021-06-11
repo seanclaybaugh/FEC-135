297634768646487535d10346b7b27007cd9b10bc
Iris <irisxjiang@gmail.com>
2021-06-11 14:39:00 -0700

    refactor addAnswerForm to use hooks

--- client/src/components/product-questions/questionsList/AddAnswerForm.jsx
+++ client/src/components/product-questions/questionsList/AddAnswerForm.jsx
@@ -1,38 +1,28 @@
-import React from 'react';
+import React, { useState } from 'react';
 import axios from 'axios';
 import SharedStyles from '../SharedStyles';
-class AddAnswerForm extends React.Component {
 
-  constructor(props) {
-    super(props);
+const AddAnswerForm = props => {
 
-    this.state = {
-      body: '',
-      name: '',
-      email: '',
-      photos: []
-    }
+  const [body, setBody] = useState('');
+  const [name, setName] = useState('');
+  const [email, setEmail] = useState('');
+  const [photos, setPhotos] = useState([]);
 
-    this.handleSubmitAnswer = this.handleSubmitAnswer.bind(this);
-    this.handleFormChange = this.handleFormChange.bind(this);
-  }
-
-  async handleSubmitAnswer(event) {
+  const handleSubmitAnswer = async (event) => {
 
     event.preventDefault();
 
     const answer = {
-      body: this.state.body,
-      name: this.state.name,
-      email: this.state.email,
-      photos: this.state.photos
+      body: body,
+      name: name,
+      email: email,
+      photos: photos
     }
 
-    const question_id = parseInt(this.props.questionId);
+    const question_id = parseInt(props.questionId);
     console.log('question id?')
     console.log(question_id)
-
-    console.log(this.state.body)
 
     try {
 
@@ -42,57 +32,42 @@ class AddAnswerForm extends React.Component {
     }  catch (error) {
       console.log(error)
     }
-    this.setState({
-      body: '',
-      name: '',
-      email: '',
-      photos: []
-    })
-    this.props.dismissAnswerForm();
+    setBody('');
+    setName('');
+    setEmail('');
+    setPhotos([]);
+
+    props.dismissAnswerForm();
   }
 
+  return (
+    <SharedStyles.ModalBackground>
+      <SharedStyles.ModalContent>
+        <SharedStyles.CloseButton onClick={props.dismissAnswerForm}>X</SharedStyles.CloseButton>
+        <div>Submit Your Answer</div>
+        <form onSubmit={handleSubmitAnswer}>
+          <label>Your Answer:</label>
+          <input name="body" value={body} placeholder="" onChange={e => setBody(e.target.value)}/>
+          <br/>
 
-  handleFormChange(event) {
-    this.setState({[event.target.name]: event.target.value});
-  }
+          <label>What is your nickname:</label>
+          <input name="name" value={name} placeholder="Example: jackson11!" onChange={e => setName(e.target.value)}/>
+          <br/>
 
-  render() {
-    return (
-      <SharedStyles.ModalBackground>
-        <SharedStyles.ModalContent>
-          <SharedStyles.CloseButton onClick={this.props.dismissAnswerForm}>X</SharedStyles.CloseButton>
-          <div>Submit Your Answer</div>
-          <form onSubmit={this.handleSubmitAnswer}>
-            <label>Your Answer:</label>
-            <input name="body" value={this.state.body} placeholder="" onChange={this.handleFormChange}/>
-            <br/>
+          <label>Your Email:</label>
+          <input name="email" value={email} placeholder="Example: jack@email.com" onChange={e => setEmail(e.target.value)}/>
+          <p>For authentication reasons, you will not be emailed</p>
+          <br/>
 
-            <label>What is your nickname:</label>
-            <input name="name" value={this.state.name} placeholder="Example: jackson11!" onChange={this.handleFormChange}/>
-            <br/>
+          <label>Upload Your Photos:</label>
+          <input name="photos" value={photos} placeholder="Submit Photo" onChange={e => setPhotos(e.target.value)}/>
+          <br/>
+          <button type="submit">Submit Answer</button>
+        </form>
+      </SharedStyles.ModalContent>
+    </SharedStyles.ModalBackground>
 
-            <label>Your Email:</label>
-            <input name="email" value={this.state.email} placeholder="Example: jack@email.com" onChange={this.handleFormChange}/>
-            <p>For authentication reasons, you will not be emailed</p>
-            <br/>
-
-            <label>Upload Your Photos:</label>
-            <input name="photos" value={this.state.photos} placeholder="Submit Photo" onChange={this.handleFormChange}/>
-            <br/>
-            <button type="submit">Submit Answer</button>
-          </form>
-
-
-          </SharedStyles.ModalContent>
-        </SharedStyles.ModalBackground>
-
-
-
-
-
-    )
-  }
-
+  )
 }
 
 export default AddAnswerForm;
