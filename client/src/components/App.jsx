b4cce92ff731b696b9eed7f497dc91e6f6b893fe
seanclaybaugh <sdclaybaugh@gmail.com>
2021-06-17 15:55:30 -0700

    fixed app

--- client/src/components/App.jsx
+++ client/src/components/App.jsx
@@ -5,7 +5,7 @@ import Spinner from './product-overview/spinner/LoadingSpinner';
 const ProductQuestions = lazy(() => import('./product-questions/ProductQuestions'));
 const ProductReviews = lazy(() => import('./reviews/reviewsIndex'));
 
-
+function App () {
 
   const productId = 25171;
   const [overviewLoaded, setOverviewLoaded] = useState(false);
